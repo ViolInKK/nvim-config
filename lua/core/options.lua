2dd9586fa8285375b85eb61e47529826f241ce83
Kirill Klimkin <violinkk29@gmail.com>
2023-12-30 23:24:32 +0300

    options comments

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,22 +1,29 @@
 local opt = vim.opt
 
+-- line number
 opt.number = true
 
+-- tabs & indentation
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.autoindent = true
 opt.expandtab = true
 
+-- line wrapping
 opt.wrap = true
 opt.linebreak = true
  
+-- search settings
 opt.ignorecase = true
 opt.smartcase = true
 
-opt.splitbelow = true
+-- split windows
+opt.splitbelow = true 
 
+-- clipboard
 opt.clipboard= "unnamed"
 
+-- spelling
 opt.spelllang = { "en_us" }
 opt.spell = true
 
