311eb52d096ec6b71682fb072e9489f129704b26
Kirill Klimkin <violinkk29@gmail.com>
2024-01-03 07:49:32 +0300

    move out of terminal keybinds

--- lua/core/keybinds.lua
+++ lua/core/keybinds.lua
@@ -20,6 +20,12 @@ keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
+-- move out of terminal
+keymap.set("t", "<C-w>j", "<C-\\><C-n><C-w>j") -- move out of terminal down
+keymap.set("t", "<C-w>k", "<C-\\><C-n><C-w>k") -- move out of terminal up
+keymap.set("t", "<C-w>h", "<C-\\><C-n><C-w>h") -- move out of terminal left
+keymap.set("t", "<C-w>l", "<C-\\><C-n><C-w>l") -- move out of terminal right
+
 ----------------------
 -- Plugin Keybinds
 ----------------------
